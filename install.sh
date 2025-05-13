a6f429d9d6f3e3d25f62c2954e9d68897b6f1b31
Nicholas Allam <njallam@outlook.com>
2025-05-13 19:03:26 +0100

    Make ~/.config/systemd/user if needed

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -euo pipefail
+
 if [ "$(id -u)" -eq 0 ]; then
     echo "This script should only be run as a regular user, not root."
     exit 1
 fi
 
 go install
+mkdir -p ~/.config/systemd/user
 sed "s|\$HOME|$HOME|g" "tailscale-expiry-checker.service" > ~/.config/systemd/user/tailscale-expiry-checker.service
 cp tailscale-expiry-checker.timer ~/.config/systemd/user/tailscale-expiry-checker.timer
 systemctl --user daemon-reload
